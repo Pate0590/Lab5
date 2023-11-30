343f672f6f8ef0e228423ed8cc51883f44ef8cf7
Ankit Patel <60725400+Pate0590@users.noreply.github.com>
2023-11-30 17:26:13 -0500

    docker-compose logs

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,21 @@
 # src/__init__.py
+import os # new
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+import sys
+
+
 # instantiate the app
 app = Flask(__name__)
 api = Api(app)
 # set config
-app.config.from_object('src.config.DevelopmentConfig') # new
+app_settings = os.getenv('APP_SETTINGS') # new
+app.config.from_object(app_settings) # new
 class Ping(Resource):
  def get(self):
-    return {
-    'status': 'success',
-    'message': 'pong!'
+   return {
+   'status': 'success',
+   'message': 'pong!'
  }
+ print(app.config, file=sys.stderr)
 api.add_resource(Ping, '/ping')
