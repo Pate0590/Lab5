419695474fbf118df3ca120ea1d12615afb092ea
Ankit Patel <60725400+Pate0590@users.noreply.github.com>
2023-11-30 18:49:21 -0500

    Pass test

--- src/config.py
+++ src/config.py
@@ -3,6 +3,8 @@ import os # new
 class BaseConfig:
  TESTING = False
  SQLALCHEMY_TRACK_MODIFICATIONS = False # new
+ SECRET_KEY = 'my_precious'
+
 class DevelopmentConfig(BaseConfig):
  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') # new
 class TestingConfig(BaseConfig):
